ded4718a1d4f862018efd0256b47de4e5c32840b
jiwankumar12yes <116423769+jiwankumar12yes@users.noreply.github.com>
2022-12-24 18:01:19 +0530

    Create Code : Interesting Alphabets.java

---
+++ Milestone1/Lecture 5: Patterns 1/Code : Interesting Alphabets.java
@@ -0,0 +1,59 @@
+/*
+Code : Interesting Alphabets
+Print the following pattern for the given number of rows.
+Pattern for N = 5
+E
+DE
+CDE
+BCDE
+ABCDE
+Input format :
+N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 26
+Sample Input 1:
+8
+Sample Output 1:
+H
+GH
+FGH
+EFGH
+DEFGH
+CDEFGH
+BCDEFGH
+ABCDEFGH
+Sample Input 2:
+7
+Sample Output 2:
+G
+FG
+EFG
+DEFG
+CDEFG
+BCDEFG
+ABCDEFG
+*/
+import java.util.*;
+
+public class Solution {
+	
+	public static void main(String[] args) {
+		//Your code goes here
+		Scanner s=new Scanner(System.in);
+		int n = s.nextInt();
+        int i = 1;
+        while(i<=n){
+            char val = (char)('A'+n-i);
+            int j = 1;
+            while(j<=i){
+                System.out.print(val);
+                val = (char)(val+1);
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    	}
+}
