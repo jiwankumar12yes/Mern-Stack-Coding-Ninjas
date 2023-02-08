70e12ff191fe8fc7ec68fe2dcf1ac9471c6e6294
jiwankumar12yes <116423769+jiwankumar12yes@users.noreply.github.com>
2023-02-08 20:02:04 +0530

    Create Reverse Each Word.java

---
+++ Milestone 2/Lecture 11: Strings/Reverse Each Word.java
@@ -0,0 +1,55 @@
+/*
+Reverse Each Word
+Aadil has been provided with a sentence in the form of a string as a function parameter. The task is to implement a function so as to print the sentence such that each word in the sentence is reversed.
+Example:
+Input Sentence: "Hello, I am Aadil!"
+The expected output will print, ",olleH I ma !lidaA".
+Input Format:
+The first and only line of input contains a string without any leading and trailing spaces. The input string represents the sentence given to Aadil.
+Output Format:
+The only line of output prints the sentence(string) such that each word in the sentence is reversed. 
+Constraints:
+0 <= N <= 10^6
+Where N is the length of the input string.
+
+Time Limit: 1 second
+Sample Input 1:
+Welcome to Coding Ninjas
+Sample Output 1:
+emocleW ot gnidoC sajniN
+Sample Input 2:
+Always indent your code
+Sample Output 2:
+syawlA tnedni ruoy edoc
+*/
+
+public class Solution {
+
+	public static String reverseEachWord(String str) {
+		//Your code goes here
+		 String ans="";
+        int Start=0;
+        int i=0;
+        for(;i<str.length();i++){
+            String reverseWord = "";
+            int end = i - 1;
+            if(str.charAt(i)==' '){
+              
+                
+                for(int j=Start;j<=end;j++){
+                    reverseWord=str.charAt(j)+reverseWord;
+                }
+                ans=ans+reverseWord+" ";
+                Start=i+1;
+            }
+        }
+        int end=i-1;
+                    String reverseWord="";
+                    for(int j=Start;j<=end;j++){
+                        reverseWord=str.charAt(j)+reverseWord;
+                    }
+                    ans=ans+reverseWord+" ";
+                    return ans;
+	}
+
+}
